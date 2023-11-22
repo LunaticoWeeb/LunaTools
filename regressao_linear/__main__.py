2a43881d5b1097641bd2280efb853932bbb4a6f5
LunaticoWeeb <2sa4vini@gmail.com>
2023-11-22 14:52:53 -0300

    Implementado opções de configurações

--- regressao_linear/__main__.py
+++ regressao_linear/__main__.py
@@ -1,25 +1,22 @@
 from . import regressao as rl
+import toml
+import os
 
-# TODO: 
-# - [x] resolver problema do interpretador não encontrar o arquivo
-# - [x] organizar arquivos da pasta
-# - [ ] criar um arquivo de configuração para o programa
+def main():
+
+    # Acha o diretório do script
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    # Criando o caminho para o arquivo de configuração e opções de gráfico
+    config_path = os.path.join(script_dir, 'config.toml')
+    graph_options_path = os.path.join(script_dir, 'graph_options.toml')
+
+    # Carregando o arquivo de configuração
+    config = toml.load(config_path) 
+    graph_options = toml.load(graph_options_path)
+
+    # Realizando a regressão e plotando o gráfico
+    rl.plotar_grafico(config, graph_options)
 
 if __name__ == '__main__':
-    print("Digite o nome (ou localização) do arquivo com os dados do eixo x:")
-    arq_eixo_x = input()
-    print("Digite o nome (ou localização) do arquivo com os dados do eixo y:")
-    arq_eixo_y = input()
-    eixo_x, eixo_y = rl.load_dados(arq_eixo_x, arq_eixo_y)
-    inclinacao, altura = rl.regressao_linear(eixo_x, eixo_y)
-
-    print("Digite o título do gráfico:")
-    titulo = input()
-    print("A legenda do eixo x:")
-    legenda_x = input()
-    print("A legenda do eixo y:")
-    legenda_y = input()
-    print("Digite o nome do arquivo para salvar o gráfico:")
-    nome_arq = input()
-
-    rl.plotar_grafico(eixo_x, eixo_y, inclinacao, altura, titulo, legenda_x, legenda_y, nome_arq)
+    main()
--- regressao_linear/regressao.py
+++ regressao_linear/regressao.py
@@ -7,26 +7,55 @@ def load_dados(arq_eixo_x, arq_eixo_y):
     y_data = np.loadtxt(arq_eixo_y)
     return x_data, y_data
 
-def regressao_linear(x_data, y_data):
+def regressao(x_data, y_data, grau):
     """Calcula a regressão linear dos dados"""
-    a, b = np.polyfit(x_data, y_data, 1)
-    return a, b
+    coef = np.polyfit(x_data, y_data, grau)
+    linha_x = np.linspace(min(x_data), max(x_data), 1000)
 
-def plotar_grafico(x_data, y_data, a, b, titulo, legenda_x, legenda_y, nome_arq):
-    """Plota o gráfico dos dados e da regressão linear"""
+    print(f'Coeficientes da regressão: {coef}')
+
+    linha_y = np.zeros(len(linha_x))
+
+    for i in range(grau+1):
+        j = grau - i
+        linha_y += coef[j]*linha_x**i
+        
+    return linha_x, linha_y
+
+
+def plotar_grafico(config, graph_options):
+    """Plota o gráfico dos dados e da regressão"""
+
+    # Carregando os dados
+    arq_eixo_x = config['arquivos']['coord_x']
+    arq_eixo_y = config['arquivos']['coord_y']
+    eixo_x, eixo_y = load_dados(arq_eixo_x, arq_eixo_y)
+
+    # Calculando a regressão
+    grau = config['grau']['grau']
+    coeficientes = regressao(eixo_x, eixo_y, grau)
+
+    # Informações do gráfico
+    titulo = config['legendas']['titulo']
+    legenda_x = config['legendas']['eixo_x']
+    legenda_y = config['legendas']['eixo_y']
+    nome_arq = config['alvo']['alvo']
+
+    # Estilo do gráfico
+    plt.style.use(graph_options['style'][config['estilo']['estilo']])
+    dados_estilo = graph_options['markers'][config['estilo']['tipo_marca']] + graph_options['colors'][config['estilo']['cor_dados']]
+    linha_estilo = graph_options['colors'][config['estilo']['cor_linha']]  
+
+    # Plotando o gráfico
     fig, ax = plt.subplots() # retorna uma tupla com a figura e os eixos
-    ax.plot(x_data, y_data, 'or', label='Dados Originais') # plotando os dados
-    ax.plot(x_data, a*x_data + b, '-b', label='Regressão Linear') # plotando a reta
+    ax.plot(eixo_x, eixo_y, 'or', label='Dados') # plotando os dados
+    ax.plot(coeficientes[0], coeficientes[1], '-b', label='Regressão') # plotando a reta
       
     # adicionando as legendas
     ax.set_title(titulo) # adicionando o titulo
     ax.set_xlabel(legenda_x) # adicionando o label do eixo x
     ax.set_ylabel(legenda_y) # adicionando o label do eixo y
-    ax.legend() # adicionando a legenda
-    plt.text(0.72, 0.12, f'inclinação: {a: 0.2f}\n  altura: {b: 0.2f}', 
-             transform=plt.gca().transAxes, fontsize=11,
-             verticalalignment='top', 
-             bbox=dict(boxstyle='round', facecolor='white', alpha=0.5)) # legenda dos coeficientes
+    ax.legend() # adicionando a legendas
       
     # salvando a figura
     fig.savefig(f"{nome_arq}.png", dpi=300) # dpi = resolução da imagem
